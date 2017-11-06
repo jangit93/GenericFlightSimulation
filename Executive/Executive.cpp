1258de6de486b9a74487511763d392f72f79f38f
jangit93 <33249770+jangit93@users.noreply.github.com>
2017-11-06 16:31:48 +0100

    neu Kompiliert

--- Executive/Executive.cpp
+++ Executive/Executive.cpp
@@ -15,17 +15,17 @@ int main(int argv, char* argc[])
 		readInData *test = new readInData;
 
 		//MatrixXd CL = test->readInTable("CL.txt");
-		Float64  mass = test->readInParameter("mass", "Aircraft.txt");
-		Float64  I_x = test->readInParameter("I_X", "Aircraft.txt");
-		Float64 I_y = test->readInParameter("I_Y", "Aircraft.txt");
-		Float64  I_z = test->readInParameter("I_Z", "Aircraft.txt");
-		Float64  I_zx = test->readInParameter("I_zx", "Aircraft.txt");
-		Float64  wingarea = test->readInParameter("wing_area", "Aircraft.txt");
-		Float64  wingspan = test->readInParameter("wing_span", "Aircraft.txt");
-		Float64  MAC = test->readInParameter("MAC", "Aircraft.txt");
-		Float64  X_CG = test->readInParameter("X_CG", "Aircraft.txt");
-		Float64  Y_CG = test->readInParameter("Y_CG", "Aircraft.txt");
-		Float64  Z_CG = test->readInParameter("Z_CG", "Aircraft.txt");
+		Float64  mass		= test->readInParameter("mass", "Aircraft.txt");
+		Float64  I_x		= test->readInParameter("I_X", "Aircraft.txt");
+		Float64	 I_y		= test->readInParameter("I_Y", "Aircraft.txt");
+		Float64  I_z		= test->readInParameter("I_Z", "Aircraft.txt");
+		Float64  I_zx		= test->readInParameter("I_zx", "Aircraft.txt");
+		Float64  wingarea	= test->readInParameter("wing_area", "Aircraft.txt");
+		Float64  wingspan	= test->readInParameter("wing_span", "Aircraft.txt");
+		Float64  MAC		= test->readInParameter("MAC", "Aircraft.txt");
+		Float64  X_CG		= test->readInParameter("X_CG", "Aircraft.txt");
+		Float64  Y_CG		= test->readInParameter("Y_CG", "Aircraft.txt");
+		Float64  Z_CG		= test->readInParameter("Z_CG", "Aircraft.txt");
 		
 		
 	LinearInterpolation *Interp = new LinearInterpolation;
